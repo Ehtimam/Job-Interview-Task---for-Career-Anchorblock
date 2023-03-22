f6af15918becccec47ce78d1f63aa8d0872eca56
Ehtimam <70962870+Ehtimam@users.noreply.github.com>
2023-03-22 23:01:06 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import LoginPage from './sign up/sign_up';
+// import SigninPage from './sign in/sign_in';
+// import Dashboard from './Dashboard/dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage/>
+      {/* <SigninPage/> */}
+      {/* <Dashboard/> */}
+    </div>
+  );
+}
+
+export default App;
